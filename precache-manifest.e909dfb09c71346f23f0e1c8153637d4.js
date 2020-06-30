d4a594ba1f966454691ddc8c3ea48cbe82679936
Sander Wessel <sander.wessel@live.nl>
2020-06-30 16:14:37 +0200

    Updates

---
+++ precache-manifest.e909dfb09c71346f23f0e1c8153637d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27727913ce030cc74e7323a66fad8beb",
+    "url": "/My-newapp/index.html"
+  },
+  {
+    "revision": "7a13eeb41961fdc20c6e",
+    "url": "/My-newapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8eded8f7234818dea64e",
+    "url": "/My-newapp/static/js/2.3c745b8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-newapp/static/js/2.3c745b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a13eeb41961fdc20c6e",
+    "url": "/My-newapp/static/js/main.028c3a0e.chunk.js"
+  },
+  {
+    "revision": "74cb92f8c6e2ff332315",
+    "url": "/My-newapp/static/js/runtime-main.076622c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/My-newapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
